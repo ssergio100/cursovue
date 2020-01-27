238172f9c0f0f6f3068aa3326f131b2f16b76111
sergio moreria <sergio100br@hotmail.com>
2020-01-27 17:25:45 -0200

    Novos exercicios

--- desafio-04/app.js
+++ desafio-04/app.js
@@ -1,14 +1,30 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		destaque:'destaque',
+		encolher:'encolher',
+		classe1 : 'classe1',
+		classe2 : 'classe2',
+		alterna: false,
+		condicional: false,
+		escolhida: '',
+		cor:'red',
+		tamanho : 0
 	},
 	methods: {
 		iniciarEfeito() {
-
+			
+			setInterval(() => {
+				this.alterna = !this.alterna
+			}, 1000);
 		},
 		iniciarProgresso() {
-
+			let set = setInterval(() => {
+				this.tamanho++
+				if(this.tamanho == 500) {
+					clearInterval(set)
+				}
+			}, 1);
 		}
 	}
 })
