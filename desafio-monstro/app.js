e9be20587c3850d7204b1c6274a832a0143ee5d4
sergio moreira <sergio100br@hotmail.com>
2020-01-29 01:38:44 -0300

    exercicio monstro

--- desafio-monstro/app.js
+++ desafio-monstro/app.js
@@ -1,13 +1,13 @@
 new Vue({
 	el: '#jogo',
 	data: {
-		width:0
+		width_jogador:0,
+		width_monstro:0
 	},
 	methods:{
 		atualizaBarra() {
-			
-			this.width++
-			
+			this.width++ 
+			console.log
 		}
 	}
 });
